b1459d1a0d3917dcb9e091b4e19de6f6e7d7ed7e
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-15 00:34:08 -0500

    First commit. Utility functions and variables.

---
+++ src/util.cpp
@@ -0,0 +1,46 @@
+//
+// Created by frank on 14/08/21.
+//
+
+#include <iostream>
+#include <string>
+#include <Magick++.h>
+#include <cmath>
+#include "util.h"
+
+std::ostream &operator<<(std::ostream &out, const Magick::ColorRGB &color)
+{
+    out << "rgb(" << color.red() << ", "
+        << color.green() << ", "
+        << color.blue() << ")";
+    return out;
+}
+
+namespace util
+{
+    // 70 levels of gray
+    inline const std::string characters1{R"($@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,"^`". )"};
+    // 10 levels of gray
+    inline const std::string characters2{"@%#*+=-:. "};
+    inline const std::string RESET{"\033[m"};
+
+    int percentageToRGB(double value)
+    {   // If 1.0 is 255 then x = y * 255 / 1
+        return static_cast<int>(std::round(value * 255));
+    }
+
+    std::string ansiRGB(double red, double green, double blue)
+    {
+        int r{percentageToRGB(red)};
+        int g{percentageToRGB(green)};
+        int b{percentageToRGB(blue)};
+        return ansiRGB(r, g, b);
+    }
+
+    std::string ansiRGB(int red, int green, int blue)
+    {
+        return "\033[48;2;" + std::to_string(red) + ";"
+               + std::to_string(green) + ";"
+               + std::to_string(blue) + "m";
+    }
+}
---
+++ src/util.h
@@ -0,0 +1,24 @@
+//
+// Created by frank on 14/08/21.
+//
+
+#ifndef IMG_TO_ASCII_UTIL_H
+#define IMG_TO_ASCII_UTIL_H
+
+#include <Magick++.h>
+#include <iostream>
+#include <string>
+
+std::ostream &operator<<(std::ostream &out, const Magick::ColorRGB &color);
+
+namespace util
+{
+    extern const std::string characters1;
+    extern const std::string characters2;
+    extern const std::string RESET;
+    int percentageToRGB(double);
+    std::string ansiRGB(int red, int green, int blue);
+    std::string ansiRGB(double red, double green, double blue);
+}
+
+#endif
