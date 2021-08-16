2ec77c987fac62a53b57e35e1abb5f99b1c16196
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-16 00:50:07 -0500

    Image successfully converted into ASCII art.

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,13 @@ using std::endl;
 void init(char**);
 std::unique_ptr<Magick::Image> loadImage(const std::string &path);
 void ImagetoGrayscale(Magick::Image &image);
+std::string convertImageToASCII(const Magick::Image &image, unsigned int columns, unsigned int rows,
+                                size_t cellsWidth, size_t cellsHeight);
 
 int main(int argc, char **args)
 {
     // Number of tiles in the output
-    unsigned int columns{80};
+    unsigned int columns{30};
     unsigned int rows{};
 
     try
@@ -43,6 +45,10 @@ int main(int argc, char **args)
 
         cout << "Tile width: " << cellsWidth << endl;
         cout << "Tile height: " << cellsHeight << endl;
+
+        std::string ascii = convertImageToASCII(*image, columns, rows, cellsWidth, cellsHeight);
+
+        cout << ascii << endl;
     }
     catch (Magick::Exception &error)
     {
