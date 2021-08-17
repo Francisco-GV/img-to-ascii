916bcce4a9e75b262e678ea2efa051acc4949b1c
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-16 20:45:42 -0500

    Added support to command line options.

--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,71 @@
 //
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <unistd.h>
 #include <Magick++.h>
-#include <memory>
 #include "util.h"
 
 using std::cout;
 using std::cerr;
 using std::endl;
 
-void init(char**);
-std::unique_ptr<Magick::Image> loadImage(const std::string &path);
-void ImagetoGrayscale(Magick::Image &image);
-std::string convertImageToASCII(const Magick::Image &image, unsigned int columns, unsigned int rows,
-                                size_t cellsWidth, size_t cellsHeight);
+std::string convertImageToASCII(char **argv, const std::string &file, unsigned int columns, double scale);
 
-int main(int argc, char **args)
+int main(int argc, char **argv)
 {
     // Number of tiles in the output
-    unsigned int columns{30};
+    unsigned int columns{100};
     unsigned int rows{};
+
+    double scale{0.43};
+
+    int opt;
+    while ((opt = getopt(argc, argv, ":c:s:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'c':
+            {
+                char *endptr;
+                long value{std::strtol(optarg, &endptr, 10)};
+
+                if (!endptr || value < 0) util::exitProgram("-c (columns) must be an positive integer number");
+                else columns = value;
+            } break;
+            case 's':
+            {
+                char *endptr;
+                double value{std::strtod(optarg, &endptr)};
+
+                if (!endptr || value < 0)
+                    util::exitProgram("-s (height scale) must be an positive floating point number");
+                else scale = value;
+            } break;
+            case '?':
+                cerr << "invalid option: " << optopt << endl;
+                break;
+            case ':':
+            default:
+                break;
+        }
+    }
+
+    std::string file;
+
+    if (optind < argc)
+    {
+        file = argv[optind];
+    }
 
     try
     {
-        init(args);
-
-        std::unique_ptr<Magick::Image> image = loadImage(args[1]);
-        size_t width{image->columns()};
-        size_t height{image->rows()};
-
-        cout << "Image: " << image->fileName() << endl;
-        cout << "image original size:" << width << "x" << height << endl;
-
-        ImagetoGrayscale(*image);
-        rows = util::calculateNewHeight(height, width, columns);
-
-        cout << "Columns: " << columns << endl;
-        cout << "Rows: " << rows << endl;
-
-        size_t cellsWidth{width / columns};
-        size_t cellsHeight{height / rows};
-
-        cout << "Tile width: " << cellsWidth << endl;
-        cout << "Tile height: " << cellsHeight << endl;
-
-        std::string ascii = convertImageToASCII(*image, columns, rows, cellsWidth, cellsHeight);
-
+        std::string ascii = convertImageToASCII(argv, file, columns, scale);
+        cout << "showing output..." << endl;
         cout << ascii << endl;
     }
     catch (Magick::Exception &error)
     {
-        cerr << "Caught Magick++ exception: " << error.what() << endl;
+        cerr << "Caught Magick++ exception:\n\t" << error.what() << endl;
     }
 }
