ffe1ad4dd1d089fb61be75c8a7411158699901ae
Francisco-GV <francisco_gasca@hotmail.com>
2021-08-16 20:50:07 -0500

    scale variable added to keep aspect ratio in output. Transparent background is replaced with white.

--- src/img_to_ascii.cpp
+++ src/img_to_ascii.cpp
@@ -2,14 +2,49 @@
 // Created by frank on 15/08/21.
 //
 
+#include <iostream>
 #include <memory>
 #include <Magick++.h>
 #include <string>
 #include "util.h"
 
-void init(char **args)
+void prepareImage(Magick::Image &);
+std::unique_ptr<Magick::Image> loadImage(const std::string &);
+std::string parsePixelsToASCII(const Magick::Image &, unsigned int, unsigned int, size_t, size_t);
+unsigned int getAverage(const Magick::Image &, unsigned int, unsigned int, size_t, size_t);
+
+std::string convertImageToASCII(char **argv, const std::string &file, unsigned int columns, double scale)
 {
-    Magick::InitializeMagick(*args);
+    using std::cout;
+    using std::endl;
+
+    unsigned int rows;
+
+    Magick::InitializeMagick(*argv);
+
+    std::unique_ptr<Magick::Image> image = loadImage(file);
+    size_t width{image->columns()};
+    size_t height{image->rows()};
+
+    cout << "Image: " << image->fileName() << endl;
+    cout << "image original size:" << width << "x" << height << endl;
+
+    prepareImage(*image);
+
+    size_t cellsWidth{width / columns};
+    size_t cellsHeight{static_cast<size_t>(cellsWidth / scale)};
+    // size_t cellsHeight{height / rows}; // <- warning: this doesn't keep aspect ratio in console/file output
+
+    cout << "Tile width: " << cellsWidth << endl;
+    cout << "Tile height: " << cellsHeight << endl;
+
+    rows = height / cellsHeight;
+    // rows = util::calculateNewHeight(height, width, columns); // <- warning: calculate number of rows using cellsHeight{height / rows}
+
+    cout << "Columns: " << columns << endl;
+    cout << "Rows: " << rows << endl;
+
+    return parsePixelsToASCII(*image, columns, rows, cellsWidth, cellsHeight);
 }
 
 std::unique_ptr<Magick::Image> loadImage(const std::string &path)
@@ -17,13 +52,16 @@ std::unique_ptr<Magick::Image> loadImage(const std::string &path)
     return std::make_unique<Magick::Image>(path);
 }
 
-void ImagetoGrayscale(Magick::Image &image)
+void prepareImage(Magick::Image &image)
 {
+    image.backgroundColor(Magick::Color("#fff"));
+    image.alphaChannel(Magick::RemoveAlphaChannel);
+    image.alphaChannel(Magick::OffAlphaChannel);
     image.type(Magick::GrayscaleType);
 }
 
 unsigned int getAverage(const Magick::Image &image, unsigned int columnNumber, unsigned int rowNumber,
-                  size_t cellsWidth, size_t cellsHeight)
+                        size_t cellsWidth, size_t cellsHeight)
 {
     int total{};
     int count{};
@@ -32,7 +70,7 @@ unsigned int getAverage(const Magick::Image &image, unsigned int columnNumber, u
         for (ssize_t x{0}; x < cellsWidth; x++)
         {
             Magick::ColorGray color{image.pixelColor(ssize_t(columnNumber * cellsWidth + x),
-                                                 ssize_t(rowNumber * cellsHeight + y))};
+                                                     ssize_t(rowNumber * cellsHeight + y))};
 
             total += util::percentageToRGB(color.shade());
             count++;
@@ -42,8 +80,8 @@ unsigned int getAverage(const Magick::Image &image, unsigned int columnNumber, u
     return static_cast<int>(std::round(total / count));
 }
 
-std::string convertImageToASCII(const Magick::Image &image, unsigned int columns, unsigned int rows,
-                         size_t cellsWidth, size_t cellsHeight)
+std::string parsePixelsToASCII(const Magick::Image &image, unsigned int columns, unsigned int rows,
+                               size_t cellsWidth, size_t cellsHeight)
 {
     using util::characters;
 
@@ -54,8 +92,8 @@ std::string convertImageToASCII(const Magick::Image &image, unsigned int columns
         {
             unsigned int average{getAverage(image, i, n, cellsWidth, cellsHeight)};
 
-            unsigned int characterIndex = (average * (characters.length())) / 255;
-            ascii += characters.at(characterIndex - 1);
+            unsigned int characterIndex = (average * (characters.length() - 1)) / 255;
+            ascii += characters.at(characterIndex);
         }
         ascii += "\n";
     }
